e02c533b1940f08c98b1472a8078ad84244df41e
shauryajain21 <shaurya.jain@nyu.edu>
2026-02-03 14:46:09 -0500

    Add redirect from /templates to /use-cases

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/templates",
+        destination: "/use-cases",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
